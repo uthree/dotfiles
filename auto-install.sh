4454dcea464b2c01a188f3098ddcc09b7bc538de
uthree <33905956+uthree@users.noreply.github.com>
2022-01-27 18:13:44 +0900

    fix

--- auto-install.sh
+++ auto-install.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 echo "\e[35;1mCloning dotfiles...\e[0m"
 git clone https://github.com/uthree/dotfiles ~/.dotfiles
-echo "\e[35;Done!\e[0m"
+echo "\e[35;1mDone!\e[0m"
 cd ~/.dotfiles
 ./install.sh
 
 # install colorscheme
+cd ~
 echo "\e[35;1mInstalling color scheme...\e[0m"
 mkdir ~/.vim
 cd ~/.vim
 mkdir colors
 
-echo "  \e[35;1mLDownloading color scheme...\e[0m"
+echo "  \e[35;1mDownloading color scheme...\e[0m"
 git clone https://github.com/w0ng/vim-hybrid
 mv vim-hybrid/colors/hybrid.vim ~/.vim/colors
-echo "  \e[35;Done!\e[0m"
-echo "\e[35;Done!\e[0m"
+echo "  \e[35;1mDone!\e[0m"
+echo "\e[35;1mDone!\e[0m"
 
 
 echo "Type 'exec zsh' to reload your shell and have fun!"
