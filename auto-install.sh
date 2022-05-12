4c553c13d148d4befd7c540b27521c124e71c23c
UThree <33905956+uthree@users.noreply.github.com>
2022-05-12 09:44:55 +0900

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -10,20 +10,6 @@ echo "\e[35;1mDone!\e[0m"
 cd ~/.dotfiles
 ./install.sh
 
-# install colorscheme
-cd ~
-echo "\e[35;1mInstalling color scheme...\e[0m"
-mkdir ~/.vim
-cd ~/.vim
-mkdir colors
-
-echo "  \e[35;1mDownloading color scheme...\e[0m"
-git clone https://github.com/w0ng/vim-hybrid
-mv vim-hybrid/colors/hybrid.vim ~/.vim/colors
-echo "  \e[35;1mDone!\e[0m"
-echo "\e[35;1mDone!\e[0m"
-
-
 echo "Type 'exec zsh' to reload your shell and have fun!"
 echo "Note: 'source .zshrc' is deprecated. for more info, visit:"
 echo "https://blog.mattclemente.com/2020/06/26/oh-my-zsh-slow-to-load.html"
