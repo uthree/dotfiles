b4424ef9873f54d39fdeb8f519e65f54ec985c8c
UThree <33905956+uthree@users.noreply.github.com>
2022-05-12 10:07:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,14 +32,14 @@ done
 
 # install code-minimap https://github.com/wfxr/code-minimap
 echo "\e[35;1mInstalling code minimap for vim ...\e[0m"
-if command -v cargo &> /dev/null
-then
-	echo "installing code-minimap from crates.io ..."
-    	cargo install --locked code-minimap
-elif command -v yay &> /dev/null
+if command -v yay &> /dev/null
 then
 	echo "installing code-minimap from AUR ..."
     	yay -S code-minimap
+elif command -v cargo &> /dev/null
+then
+	echo "installing code-minimap from crates.io ..."
+    	cargo install --locked code-minimap
 elif command -v brew &> /dev/null
 then
 	echo "installing code-minimap from homebrew ..."
