646bb062f0f2c2f22ad8d2f97730849da3bfde46
uthree <uthree271828@gmail.com>
2022-11-11 14:25:37 +0900

    fix

--- .zshrc.d/00-os.zsh
+++ .zshrc.d/00-os.zsh
@@ -1,9 +1,9 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   local DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
   if [[ ${DISTRIB} = "Ubuntu"* ]]; then
-    if uname -a | grep -q '^Linux.*Microsoft'; then
+    if uname -a | grep -q '^WSL'; then
       # ubuntu via WSL Windows Subsystem for Linux
-      
+      echo "\e[35;1mDetected WSL...\e[0m"
       # CUDA
       export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
     else
