090a653c080b691b21ba6ed461d95d85bc337502
uthree <uthree271828@gmail.com>
2022-11-11 14:26:12 +0900

    fix

--- .zshrc.d/00-os.zsh
+++ .zshrc.d/00-os.zsh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   local DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
   if [[ ${DISTRIB} = "Ubuntu"* ]]; then
-    if uname -a | grep -q '^WSL'; then
+    if uname -a | grep -q 'WSL'; then
       # ubuntu via WSL Windows Subsystem for Linux
       echo "\e[35;1mDetected WSL...\e[0m"
       # CUDA
