85b3cb0864519c22b9c5012fba84c54319c75e58
uthree <uthree@uthree-macbookair.local>
2022-11-24 20:25:15 +0900

    fix

--- .zshrc.d/99-trash.zsh
+++ .zshrc.d/99-trash.zsh
@@ -1,15 +1,6 @@
 if [ -d ${HOME}/.Trash ]; then
-	if [[ "$OSTYPE" == "darwin"* ]]; then
-  		# macOS OSX
-		if type trash > /dev/null 2>&1; then
-    		alias rm='trash -F'
-		else
-			echo "\e[35;1mtrash is not detected.  run brew install trash  if you need trash directory.\e[0m"
-		fi
-	else
-    	alias rm='mv --backup=numbered --target-directory=${HOME}/.Trash'
-    	alias cleartrash='command rm -rf ~/.Trash/*' 
-	fi
+	alias rm='mv --backup=numbered --target-directory=${HOME}/.Trash'
+    alias cleartrash='command rm -rf ~/.Trash/*' 
 else
 	echo
     echo "\e[35;1mTrash Directory not detected. Run mkdir ${HOME}/.Trash if you need trash directory.\e[0m"
