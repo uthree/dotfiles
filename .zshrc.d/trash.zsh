a885ace78cd5a2744494e2309162245679b34692
uthree <uthree271828@gmail.com>
2024-11-18 09:27:23 +0900

    tmp3

--- .zshrc.d/trash.zsh
+++ .zshrc.d/trash.zsh
@@ -4,7 +4,7 @@ if [ -d ${HOME}/.Trash ]; then
         alias rm='mv --backup=numbered --target-directory=${HOME}/.Trash'
         alias cleartrash='command rm -rf ~/.Trash/*'
     else
-        if type "trash" &> /dev/null; then
+        if type trash &> /dev/null; then
             alias rm='trash'
         else
             echo "\e[35;1mWarning: trash command is not installed.\e[0m"
